9f2f3e50b7443566c92a0ad1dfa02dd0c81ca553
Eskendarov <Eskendarov.Rufat@sunbury.ca>
2022-12-05 20:25:23 -0400

    Merged HTML, js-logic

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,161 +1,186 @@
+"use strict";
+
+// =================Side Nav bar=========================
+
+const dropDowns = document.querySelectorAll(".dropdown");
+const dropItems = document.querySelectorAll(".dropdown-item");
+
+for (let i = 0; i < dropDowns.length; i++) {
+  dropDowns[i].addEventListener("click", function () {
+    dropDowns[i].classList.toggle("is-active");
+    dropDowns[i].classList.toggle("drop-mar");
+  });
+}
+
+for (let i = 0; i < dropItems.length; i++) {
+  dropItems[i].addEventListener("click", function () {
+    console.log(dropItems[i].textContent);
+  });
+}
+
+// ======================================================
 
 //country appreviation is the code of 3 characters
 class CountryInfo {
-    constructor(name, apprev, flag, capital, population, languages) {
-        this.name = name;
-        this.apprev = apprev;
-        this.flag = flag;
-        this.capital = capital;
-        this.population = population;
-        this.languages = languages; //languages is an array
-    }
+  constructor(name, apprev, flag, capital, population, languages) {
+    this.name = name;
+    this.apprev = apprev;
+    this.flag = flag;
+    this.capital = capital;
+    this.population = population;
+    this.languages = languages; //languages is an array
+  }
 }
 
-
 class TeamStats {
-    constructor(team, wins, losses, draws, points) {
-        this.team = team;
-        this.wins = wins;
-        this.losses = losses;
-        this.draws = draws;
-        this.points = points;
-    }
+  constructor(team, wins, losses, draws, points) {
+    this.team = team;
+    this.wins = wins;
+    this.losses = losses;
+    this.draws = draws;
+    this.points = points;
+  }
 }
 
 class Group {
-    constructor(groupCode, teams) {
-        this.groupCode = groupCode;
-        this.teams = teams;
-    }
+  constructor(groupCode, teams) {
+    this.groupCode = groupCode;
+    this.teams = teams;
+  }
 }
 
 //country name is in 3 characters
 
 class Match {
-    constructor(homeCountryName, homeCountryApprev, homeFlag, homeCountryGoals, awayCountryName, awayCountryApprev, awayFlag, awayCountryGoals, date, stage) {
+  constructor(
+    homeCountryName,
+    homeCountryApprev,
+    homeFlag,
+    homeCountryGoals,
+    awayCountryName,
+    awayCountryApprev,
+    awayFlag,
+    awayCountryGoals,
+    date,
+    stage
+  ) {
+    this.homeCountryName = homeCountryName;
+    this.homeCountryApprev = homeCountryApprev;
+    this.homeCountryGoals = homeCountryGoals;
+    this.homeFlag = homeFlag;
 
-        this.homeCountryName = homeCountryName;
-        this.homeCountryApprev = homeCountryApprev;
-        this.homeCountryGoals = homeCountryGoals;
-        this.homeFlag = homeFlag;
+    this.awayCountryName = awayCountryName;
+    this.awayCountryGoals = awayCountryGoals;
+    this.awayCountryApprev = awayCountryApprev;
+    this.awayFlag = awayFlag;
 
-        this.awayCountryName = awayCountryName
-        this.awayCountryGoals = awayCountryGoals;
-        this.awayCountryApprev = awayCountryApprev;
-        this.awayFlag = awayFlag;
-
-        this.date = date;
-        this.stage = stage;
-
-    }
+    this.date = date;
+    this.stage = stage;
+  }
 }
-
 
 function getGroupStats() {
-    var reqURL = "https://copa22.medeiro.tech/groups";
-    var allGroups = [];
+  var reqURL = "https://copa22.medeiro.tech/groups";
+  var allGroups = [];
 
-    fetch(reqURL)
-        .then(function (response) {
+  fetch(reqURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      //for each group
+      for (var i = 0; i < data.length; i++) {
+        var teams = [];
 
-            return response.json();
-        })
-        .then(function (data) {
-
-            //for each group
-            for (var i = 0; i < data.length; i++) {
-
-                var teams = [];
-
-                for (var j = 0; j < data[i]["teams"].length; j++) {
-                    teams.push(new TeamStats(data[i]["teams"][j]["country"],
-                        data[i]["teams"][j]["wins"],
-                        data[i]["teams"][j]["losses"],
-                        data[i]["teams"][j]["draws"],
-                        data[i]["teams"][j]["points"]));
-                }
-                allGroups.push(new Group(data[i]["code"], teams));
-            }
-
-        });
-    return allGroups;
+        for (var j = 0; j < data[i]["teams"].length; j++) {
+          teams.push(
+            new TeamStats(
+              data[i]["teams"][j]["country"],
+              data[i]["teams"][j]["wins"],
+              data[i]["teams"][j]["losses"],
+              data[i]["teams"][j]["draws"],
+              data[i]["teams"][j]["points"]
+            )
+          );
+        }
+        allGroups.push(new Group(data[i]["code"], teams));
+      }
+    });
+  return allGroups;
 }
-
-
 
 //country name should be the full name not the country appreviation
 function getCountryInfo(countryName) {
-    var requestUrl = 'https://restcountries.com/v2/name/' + countryName;
+  var requestUrl = "https://restcountries.com/v2/name/" + countryName;
 
-    var countryInfo = [];
-    fetch(requestUrl)
-        .then(function (response) {
+  var countryInfo = [];
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      for (var i = 0; i < data.length; i++) {
+        var languages = [];
+        for (var j = 0; j < data[i]["languages"].length; j++) {
+          languages.push(data[i]["languages"][j]["name"]);
+        }
+        var tempCountryInfo = new CountryInfo(
+          data[i]["name"],
+          data[i]["alpha3Code"],
+          data[i]["flags"]["png"],
+          data[i]["capital"],
+          data[i]["population"],
+          languages
+        );
 
-            return response.json();
-        })
-        .then(function (data) {
-            for (var i = 0; i < data.length; i++) {
-                var languages = [];
-                for (var j = 0; j < data[i]["languages"].length; j++) {
-                    languages.push(data[i]["languages"][j]["name"]);
-                }
-                var tempCountryInfo = new CountryInfo(
-                    data[i]["name"],
-                    data[i]["alpha3Code"],
-                    data[i]["flags"]["png"],
-                    data[i]["capital"],
-                    data[i]["population"],
-                    languages);
+        countryInfo.push(tempCountryInfo);
+      }
+    });
 
-                countryInfo.push(tempCountryInfo);
-            }
-
-        });
-
-    return countryInfo;
+  return countryInfo;
 }
 
 //the country name should be in 3 characters
 function getMatchesByCountry(countryName) {
-    var matchesByCountry = [];
-    var reqURL = "https://copa22.medeiro.tech/teams/" + countryName + "/matches";
-    fetch(reqURL)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
+  var matchesByCountry = [];
+  var reqURL = "https://copa22.medeiro.tech/teams/" + countryName + "/matches";
+  fetch(reqURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      //for each match
+      for (var i = 0; i < data.length; i++) {
+        var homeCountryName = data[i]["homeTeam"]["name"];
+        var homeCountryApprev = data[i]["homeTeam"]["country"];
+        var homeCountryGoals = data[i]["homeTeam"]["goals"];
+        var homeCountryInfo = getCountryInfo(homeCountryName);
+        var homeFlag = "";
 
-            //for each match
-            for (var i = 0; i < data.length; i++) {
-                var homeCountryName = data[i]["homeTeam"]["name"];
-                var homeCountryApprev = data[i]["homeTeam"]["country"];
-                var homeCountryGoals = data[i]["homeTeam"]["goals"];
-                var homeCountryInfo = getCountryInfo(homeCountryName);
-                var homeFlag = "";
+        var awayCountryName = data[i]["awayTeam"]["name"];
+        var awayCountryApprev = data[i]["awayTeam"]["country"];
+        var awayCountryGoals = data[i]["awayTeam"]["goals"];
+        var awayFlag = "";
 
-                var awayCountryName = data[i]["awayTeam"]["name"];
-                var awayCountryApprev = data[i]["awayTeam"]["country"];
-                var awayCountryGoals = data[i]["awayTeam"]["goals"];
-                var awayFlag = "";
+        matchesByCountry.push(
+          new Match(
+            homeCountryName,
+            homeCountryApprev,
+            homeFlag,
+            homeCountryGoals,
+            awayCountryName,
+            awayCountryApprev,
+            awayFlag,
+            awayCountryGoals,
+            data[i]["date"],
+            data[i]["stageName"]
+          )
+        );
+      }
+    });
 
-
-                matchesByCountry.push(new Match(homeCountryName,
-                    homeCountryApprev,
-                    homeFlag,
-                    homeCountryGoals,
-                    awayCountryName,
-                    awayCountryApprev,
-                    awayFlag,
-                    awayCountryGoals,
-                    data[i]["date"],
-                    data[i]["stageName"]));
-
-            }
-        });
-
-    return matchesByCountry;
+  return matchesByCountry;
 }
-
 
 var country = getCountryInfo("canada");
 console.log("============contry info==============");
